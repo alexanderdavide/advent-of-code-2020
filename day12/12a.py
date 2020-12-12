dd7ecc21ca4c82464b008b6b178fe5408f2a5934
Alexander Eble <alexeble1998@gmail.com>
2020-12-12 11:59:48 +0100

    add day12

---
+++ day12/12a.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+
+def make_deg_between_0_360(deg):
+    if deg >= 360:
+        return deg - 360
+    elif deg < 0:
+        return deg + 360
+
+    return deg
+
+
+def pilot_boat(lines):
+    facing_to_deg = {"N": 0, "E": 90, "S": 180, "W": 270}
+
+    curr_ship_deg = 90
+    manhattan = {90: 0, 270: 0, 0: 0, 180: 0}
+
+    for line in lines:
+        direc = line[0]
+        change = int(line[1:])
+
+        if direc == "F":
+            manhattan[curr_ship_deg] += change
+        elif direc == "R":
+            curr_ship_deg = make_deg_between_0_360(curr_ship_deg + change)
+        elif direc == "L":
+            curr_ship_deg = make_deg_between_0_360(curr_ship_deg - change)
+        else:
+            manhattan[facing_to_deg[direc]] += change
+
+    return abs(manhattan[90] - manhattan[270]) + abs(manhattan[0] - manhattan[180])
+
+
+def main():
+    with open("12.txt") as input:
+        lines = input.readlines()
+        lines = reduce(lambda acc, el: [*acc, el.rstrip("\n")], lines, [])
+        print(pilot_boat(lines))
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
